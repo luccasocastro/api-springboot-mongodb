d1bf211a66ad9a489f4f89891ae661fc12035a52
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-03-14 18:37:43 -0300

    conectando ao db

---
+++ src/main/java/com/luxkapotter/Api/Mongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.luxkapotter.Api.Mongo.repositories;
+
+import com.luxkapotter.Api.Mongo.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
